f97bee248f62cd4c25a205a215142bc0548e003b
els_gamecraft <elysia.aurora@gmail.com>
2023-09-25 16:45:03 +0700

    progress Raptor Run game - Lesson 6

---
+++ scripts/game.gd
@@ -0,0 +1,78 @@
+extends Node2D
+
+@export var world_speed := 300
+@export var collectible_pitch_reset_interval := 2000
+
+@onready var moving_environment = $Environment/Moving
+@onready var collect_sound = $Sounds/CollectSound
+@onready var score_label = $HUD/UI/Score
+
+
+var platform = preload("res://scenes/platform.tscn")
+var platform_collectible_single := preload("res://scenes/platform_collectible_single.tscn")
+var platform_collectible_row := preload("res://scenes/platform_collectible_row.tscn")
+var platform_collectible_rainbow := preload("res://scenes/platform_collectible_rainbow.tscn")
+var rng = RandomNumberGenerator.new()
+var last_platform_position := Vector2.ZERO
+var next_spawn_time := 0
+var score := 0
+var collectible_pitch := 1.0
+var reset_collectible_pitch_time := 0
+
+#Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+
+	
+#Called evry frame. 'delta' is elapsed time since the previous frame.
+func _process(delta):
+	#Reset the collectible sound pitch after a time
+	if Time.get_ticks_msec() > reset_collectible_pitch_time:
+		collectible_pitch = 1.0
+	
+	#Spawn in a new platform
+	if Time.get_ticks_msec() > next_spawn_time:
+		_spawn_next_platform()
+		
+		#Update the UI labels
+		score_label.text = "Score: %s" % score
+		
+func _spawn_next_platform():
+	var availible_platforms = [
+		platform,
+		platform_collectible_single,
+		platform_collectible_row,
+		platform_collectible_rainbow
+	]
+	var platform_index = rng.randi_range(0, availible_platforms.size() - 1)
+
+	var new_platform = availible_platforms[platform_index].instantiate()
+		
+	#Set the position of the new platform
+	if last_platform_position == Vector2.ZERO:
+		new_platform.position = Vector2(400, 0)
+	else:
+		var x = last_platform_position.x + rng.randi_range(450, 550)
+		var y = clamp(last_platform_position.y + rng.randi_range(-150, 150),  200, 1000)
+		new_platform.position = Vector2(x, y)
+		
+	#Add the platform to the moving environment
+	moving_environment.add_child(new_platform)
+	
+	#Update the last platform position and increase the next spawn
+	last_platform_position = new_platform.position
+	next_spawn_time += world_speed
+
+
+func _physics_process(delta):
+	#Move the platform left
+	moving_environment.position.x -= world_speed * delta
+	
+func add_score(value):
+	score += value
+	collect_sound.set_pitch_scale(collectible_pitch)
+	collect_sound.play()
+	collectible_pitch += 0.1
+	reset_collectible_pitch_time = Time.get_ticks_msec() + collectible_pitch_reset_interval
+	# print(score)
+	
